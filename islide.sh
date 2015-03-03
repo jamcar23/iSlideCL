62ac2454907c03ee4077603b478f7888291be095
jamcar23 <jamcar23@gmail.com>
2015-03-03 14:50:09 -0500

    upload & set permissions; uninstall

--- islide.sh
+++ islide.sh
@@ -7,16 +7,58 @@ cat << EOF
 EOF
 }
 
+upload_app() {
+	set +v
+	echo `scp -r $a root@$ip:/Applications/`
+	set -v
+}
+
+set_app() {
+	local loc_app=""
+	
+	loc_app=${a:0: ${#a} - 5}
+	
+	ssh root@$ip '
+	printf "Connecting...           done.\n"
+		
+	cd /var/mobile/
+	chmod +x /Applications/$a/$loc_app
+	printf "Setting permission...   done.\n"
+		
+	uicache
+	printf "Clearing cache...       done.\n"
+		
+	killall -HUP SpringBoard
+	printf "Respringing...          done.\n"
+	exit 0
+	exit 0
+		'
+}
+
+uninstall() {
+	ssh root@$ip '
+	rm -rf /Applications/$u/
+	printf "Uninstalling app...     done.\n"
+	
+	uicache
+	printf "Clearing cache...       done.\n"
+		
+	killall -HUP SpringBoard
+	printf "Respringing...          done.\n"
+	exit 0
+	'
+}
+
 # Variables
 ip=""
-f=""
-pass=""
+a=""
+u=""
 OPTIND=1
-while getopts ":f:hi:p:" opt; do
+while getopts ":a:hi:p:u:" opt; do
 	case "$opt" in 
-		f)
-			f=$OPTARG
-			echo "-f $f"
+		a)
+			a=$OPTARG
+			echo "-a $a"
 			;;
 		h)
 			show_help
@@ -28,6 +70,10 @@ while getopts ":f:hi:p:" opt; do
 			;;
 		p)
 			pass=$OPTARG
+			;;
+		u)
+			u=$OPTARG
+			echo "-u $u"
 			;;
 		\?)
 			echo "Invalid option: -$OPTARG"
@@ -43,15 +89,30 @@ while getopts ":f:hi:p:" opt; do
 done
 shift "$((OPTIND-1))"
 
-if [ -n "$f" ] && [ -n "$ip" ]; then
-	printf "Transferring app... " 
-	set +v
-	scp -r $f root@$ip:/Applications/
-	set -v
-	printf "done.\n"
+if [ -n "$a" ] && [ -n "$ip" ]; then
 	
 	
+	printf "Transferring app... \n" 
+	upload_app
+	printf "Transferring app...     done.\n"
+	
+	#TODO: Integrity check
+	
+	printf "\nConnecting... "
+	printf "Setting permission... "
+	printf "Clearing cache... "
+	printf "Respringing... "
+	set_app
+
+	exit 0 
+elif [ -n "$u" ]; then 
+	printf "\nUninstalling app..."
+	printf "Clearing cache... "
+	printf "Respringing... "
+	uninstall
+	
+	exit 0
 else
-	echo "-f and -i are required arguments"
+	echo "-a and -i are required arguments"
 	exit 0
 fi
