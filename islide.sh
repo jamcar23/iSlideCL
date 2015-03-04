848976857726994a212560c398f45ee6e9976790
jamcar23 <jamcar23@gmail.com>
2015-03-03 23:03:29 -0500

    dev merge

--- islide.sh
+++ islide.sh
@@ -1,79 +1,131 @@
 #!/bin/bash
 
 # Usage info
-show_help() {
+help() {
 cat << EOF
-# Welcome to iSlideCL            v0.1
-#            James Carroll (jamcar23)
+# Welcome to iSlideCL                      v0.1
+# Copyright GPLv3 James Carroll (jamcar23) 2015
 #
-# Usuage: $(basename $0) [-h] [-a appName.app] [-u appName.app] [-i IP Addrress]
+# Usuage: $(basename $0) [-ch] [-a appName.app] [-u appName.app] [-i IP Addrress]
 # *You MUST have OpenSSH installed on your iDevice*
 #
 # commands:
 #     -h    show this help text
+#     -c    show copyright 
 #     -a    App to be installed
 #     -u    App to be uninstalled
 #     -i    IP Address
 EOF
 }
 
+#Copyright info
+copyright() {
+cat << EOF
+# Copyright James Carroll (jamcar23) 2015
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+EOF
+}
+
+#Uploads .app file using scp -r
 upload_app() {
+  local loc_a="$1"
+	
+  printf "Transferring app... \n"
   set +v
   echo `scp -r $a root@$ip:/Applications/`
   set -v
+  printf "Transferring app...            done.\n"
 }
 
+app_integ() {
+	local loc_zip="${a}.gz"
+
+	gzip -cr "$a" > "${a}.gz"
+	hash=$(md5 "$loc_zip")
+	
+	upload_app "$loc_zip"
+	
+
+}
+
+#Sets the app up so it can be used 
 set_app() {
   local loc_app=""
-	
-  loc_app=${a:0: ${#a} - 5}
-	
+  local loc_zip="${a}.gz"
+  local loc_hash=""
+  
+  loc_app=${a:0: ${#a} - 4}
+
+  printf "\nSetting up app...\n"	
   ssh root@$ip '
-    printf "Connecting...           done.\n"
-		
+    printf "\nConnecting...                  done.\n"
+    	
     cd /var/mobile/
     chmod +x /Applications/$a/$loc_app
-    printf "Setting permission...   done.\n"
+    printf "Setting permission...          done.\n"
 		
     uicache
-    printf "Clearing cache...       done.\n"
+    printf "Clearing cache...              done.\n"
 		
     killall -HUP SpringBoard
-    printf "Respringing...          done.\n"
+    printf "Respringing...                 done.\n"
     
     exit 0
   '
 }
 
+#Removes the app 
 uninstall() {
-  ssh root@$ip '
-    rm -rf /Applications/$u/
-    printf "Uninstalling app...     done.\n"
+  printf "Uninstalling... \n"
+  ssh root@$ip "
+    printf '\nConnecting...                  done.\n'
+
+    rm -rf /Applications/$u
+    printf 'Uninstalling app...            done.\n'
 	
     uicache
-    printf "Clearing cache...       done.\n"
+    printf 'Clearing cache...              done.\n'
 		
     killall -HUP SpringBoard
-    printf "Respringing...          done.\n"
+    printf 'Respringing...                 done.\n'
     
     exit 0
-  '
+  "
 }
 
 # Variables
 ip=""
 a=""
 u=""
+hash=""
 OPTIND=1
 
-while getopts ":a:hi:p:u:" opt; do
+while getopts ":a:chi:p:u:" opt; do
   case "$opt" in 
     a)
       a=$OPTARG
       ;;
+    c)
+      copyright
+      
+     
+      ;;
     h)
-      show_help
-      exit 0
+      help
+      
+     
       ;;
     i)
       ip=$OPTARG
@@ -86,7 +138,7 @@ while getopts ":a:hi:p:u:" opt; do
       ;;
     \?)
       echo "Invalid option: -$OPTARG"
-      show_help
+      help
       
       exit 0
       ;;
@@ -100,28 +152,17 @@ done
 shift "$((OPTIND-1))"
 
 if [ -n "$a" ] && [ -n "$ip" ]; then
-  printf "Transferring app... \n" 
   upload_app
-  printf "Transferring app...     done.\n"
   
   #TODO: Integrity check
-  
-  printf "\nConnecting... "
-  printf "Setting permission... "
-  printf "Clearing cache... "
-  printf "Respringing... "
+
   set_app
   
   exit 0 
-elif [ -n "$u" ]; then 
-  printf "\nUninstalling app..."
-  printf "Clearing cache... "
-  printf "Respringing... "
+elif [ -n "$u" ] && [ -n "$ip" ]; then 
   uninstall
   
   exit 0
 else
-  echo "-a and -i are required arguments"
-  
   exit 0
 fi
